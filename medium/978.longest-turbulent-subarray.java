aa027ecb16339b79b37e70eab0421726ca24c0ce
Xiaowei Tong <willtxw@gmail.com>
2021-05-20 23:46:10 -0700

    992

---
+++ medium/978.longest-turbulent-subarray.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=978 lang=java
+ *
+ * [978] Longest Turbulent Subarray
+ */
+
+// @lc code=start
+class Solution {
+    public int maxTurbulenceSize(int[] arr) {
+        
+    }
+}
+// @lc code=end
+
