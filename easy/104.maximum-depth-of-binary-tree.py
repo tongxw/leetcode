ac86c3dd02cab9a18a13c89596f53d3cf0e3bb01
Will(Xiaowei) Tong <willtxw@gmail.com>
2021-05-21 16:19:06 -0700

    104

---
+++ easy/104.maximum-depth-of-binary-tree.py
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode id=104 lang=python3
+#
+# [104] Maximum Depth of Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        # bfs
+        queue = [root]
+        depth = 0
+        while queue:
+            cur_level = len(queue)
+            for i in range(cur_level):
+                node = queue.pop(0)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            depth += 1
+
+        return depth
+        
+    
+
+        
+# @lc code=end
+
