99ce6cbdc3c333eeac99555cad5886cabd591a66
Will(Xiaowei) Tong <willtxw@gmail.com>
2021-05-21 17:28:22 -0700

    978

--- medium/978.longest-turbulent-subarray.java
+++ medium/978.longest-turbulent-subarray.java
@@ -7,7 +7,47 @@
 // @lc code=start
 class Solution {
     public int maxTurbulenceSize(int[] arr) {
+        if (arr.length <= 1) {
+            return arr.length;
+        }
+
+        int l = 0;
+        int ans = 0;
+        int r = 1;
+        for (; r<arr.length; r++) {
+            if (!check(arr, l, r)) {
+                // System.out.println("l: " + l + " r: " + r);
+                ans = Math.max(ans,  r - l);
+                l = r - 1;
+            }
+        }
+
+        ans = Math.max(ans,  r - l);
+
+        // [0,8,45,88,48,68,28,55,17,24]
+        // [9, 9]
+        return ans;
         
+    }
+
+    private boolean check(int[] arr, int l, int r) {
+        int lastSign = 0; // 1 => arr[i] > arr[i-1] -1 => arr[i] < arr[i-1]
+        for (int i=l; i<=r; i++) {
+            if (i > l) {
+                if (arr[i] == arr[i-1]) {
+                    return false;
+                }
+                if ((arr[i] > arr[i-1] && lastSign == 1) || (arr[i] < arr[i-1] && lastSign == -1)) {
+                    return false;
+                } else {
+                    lastSign = arr[i] > arr[i-1] ? 1 : -1;
+                }
+            }
+        }
+
+
+
+        return true;
     }
 }
 // @lc code=end
