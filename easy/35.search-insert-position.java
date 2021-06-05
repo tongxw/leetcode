f9bc25e12ff1d08c0b4b90516c8118c225707d6a
Will(Xiaowei) Tong <willtxw@gmail.com>
2021-06-04 18:05:46 -0700

    35

---
+++ easy/35.search-insert-position.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=35 lang=java
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        // no need to check this...
+        // if (target < nums[0]) {
+        //     return 0;
+        // }
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
+// @lc code=end
+
