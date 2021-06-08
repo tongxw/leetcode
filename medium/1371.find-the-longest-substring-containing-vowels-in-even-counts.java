3e6aa0d75c1094a77f9cf7b80739b10ee91c8959
Xiaowei Tong <willtxw@gmail.com>
2021-06-07 23:11:23 -0700

    743

--- medium/1371.find-the-longest-substring-containing-vowels-in-even-counts.java
+++ medium/1371.find-the-longest-substring-containing-vowels-in-even-counts.java
@@ -11,11 +11,47 @@ class Solution {
     public int findTheLongestSubstring(String s) {
 
         // return bruteForce(s);
-        return preSum(s);
+        // return preSum(s);
+        return preSum2(s);
     }
 
+    // TODO review
     private int preSum2(String s) {
-        
+        char[] chars = s.toCharArray();
+
+        // 将 5个元音字母出现次数的奇偶视为一种状态，一共有32种状态，不妨使用一个整数代表状态：
+        // 第0位为1表示a出现奇数次，第1位为1表示e出现奇数次……以此类推。仅有状态0符合题意。
+        int[] vowStatusMap = new int[32];
+        Arrays.fill(vowStatusMap, -1);
+
+        //需要注意状态0 首次出现的位置应该设定为 -1
+        vowStatusMap[0] = -1;
+        int curStatus = 0;
+        int ans = 0;
+
+        // 而如果子串 [0，i] 与字串 [0,j] 状态相同，那么字串 [i+1,j] 的状态一定是0
+        // 因此可以记录每个状态第一次出现的位置，此后再出现该状态时相减即可
+        for (int i=0; i<chars.length; i++) {
+            char c = chars[i];
+            switch (c) {
+                // 在计算状态的时候可以利用异或运算
+                case 'a': curStatus ^= 1; break;
+                case 'e': curStatus ^= 2; break;
+                case 'i': curStatus ^= 4; break;
+                case 'o': curStatus ^= 8; break;
+                case 'u': curStatus ^= 16; break;
+                default: break;
+            }
+            if (curStatus == 0) {
+                ans = Math.max(ans, i+1);
+            } else if (vowStatusMap[curStatus] == -1) {
+                vowStatusMap[curStatus] = i;
+            } else {
+                ans = Math.max(ans, i - vowStatusMap[curStatus]);
+            }
+        }
+
+        return ans;
     }
 
     // time O(n2)
--- medium/743.network-delay-time.java
+++ medium/743.network-delay-time.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+
 /*
  * @lc app=leetcode id=743 lang=java
  *
@@ -8,28 +12,127 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         // create graph
-        int[][] graph = new int[n][n];
+        int infiniteCost = 6200; // times.length <= 6000 and n <= 100
+        int[][] graph = new int[n+1][n+1];
+        int[] dist = new int[n+1];
+        for (int i=1; i<=n; i++) {
+            dist[i] = infiniteCost;
+            for (int j=1; j<=n; j++) {
+                graph[i][j] = infiniteCost;
+            }
+        }
         for (int[] time : times) {
-            int from = time[0] - 1;
-            int to = time[1] - 1;
-            int weight = time[2];
-            graph[from - 1][to - 1] = weight;
+            int from = time[0];
+            int to = time[1];
+            int cost = time[2];
+
+            graph[from][to] = cost;
+            if (from == k) {
+                dist[to] = cost; // set the dist to k
+            }
         }
 
-        // dfs graph?
-        int maxTime = 0;
+        dist[k] = 0;
+        boolean[] visited = new boolean[n+1];
+        visited[k] = true;
 
-        int[] nodeK = graph[k-1];
-        for (int i=0; i<n; i++) {
-            maxTime = Math.max(maxTime, getTime(i));
+        // visit all nodes
+        for (int i=1; i<=n; i++) {
+            int minCost = Integer.MAX_VALUE;
+            int nodeToVisit = -1;
 
+            // find the closest node which is not visited
+            for (int j=1; j<dist.length; j++) {
+                if (visited[j]) {
+                    continue;
+                }
+
+                if (dist[j] < minCost) {
+                    minCost = dist[j];
+                    // System.out.println(i + ": "  + j + ": " + minCost);
+                    nodeToVisit = j;
+                }
+            }
+
+            if (nodeToVisit == -1) {
+                break;
+            } else {
+                visited[nodeToVisit] = true;
+            }
+
+            for (int m=1; m<graph[nodeToVisit].length; m++) {
+                if (visited[m]) {
+                    continue;
+                }
+
+                // update the shorted path from k to m
+                int cost = graph[nodeToVisit][m];
+                dist[m] = Math.min(dist[m],  dist[nodeToVisit] + cost);
+            }
         }
 
-        return maxTime;
+        int ans = 0;
+        for (int i=1; i<dist.length; i++) {
+            ans = Math.max(ans, dist[i]);
+        }
+
+        return ans  == infiniteCost ? -1 : ans;
     }
 
-    private int getTime(int[][] graph, int from, int to) {
+    private int dijkstra(ArrayList<int[]>[] graph, int from, int to) {
+        if (from == to) {
+            return 0;
+        }
+
+        PriorityQueue<int[]> pQ = new PriorityQueue<>((info1, info2) -> {
+            return info1[1] - info2[1];
+        });
+
+        pQ.offer(new int[]{from, 0});
+        HashSet<Integer> visited = new HashSet<>();
+        while (!pQ.isEmpty()) {
+            int[] info = pQ.poll();
+            int fromNode = info[0];
+            int weight = info[1];
+
+            if (visited.contains(fromNode)) {
+                continue;
+            }
+            visited.add(fromNode);
+
+            if (fromNode == to) {
+                return weight;
+            }
+
+            ArrayList<int[]> node = graph[fromNode];
+            for (int[] e : node) {
+                if (visited.contains(e[0])) {
+                    int newWeight = weight + e[1];
+                    pQ.add(new int[]{e[0], newWeight});
+                }
+            }
+        }
+
         
+        return -1;
+
+
+
+        // heap = [(0, start)]
+        // visited = set()
+        // while heap:
+        //     (cost, u) = heapq.heappop(heap)
+        //     if u in visited:
+        //         continue
+        //     visited.add(u)
+        //     if u == end:
+        //         return cost
+        //     for v, c in graph[u]:
+        //         if v in visited:
+        //             continue
+        //         next = cost + c
+        //         heapq.heappush(heap, (next, v))
+        // return -1
     }
 }
 // @lc code=end
